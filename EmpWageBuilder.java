2191016cbf17e9397815be516645ef88b3d7bcfb
Ayush <vayush198@gmail.com>
2020-05-06 18:13:16 +0530

    Add:Added UC-3, add part time Employee and wage condition

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,16 +1,22 @@
 public class EmpWageBuilder{
-	public static void main(String [] args) {
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
+	public static int IS_FULL_TIME = 2;
+	public static int EMP_RATE_PER_HOUR = 20;
+	public static int IS_PART_TIME = 1;
+
+	public static void main (String[] args) {
 
 		int empHours = 0;
 		int employeeWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 
 		if(empCheck==IS_FULL_TIME){
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present for full time");
 			empHours=8;
+		}
+		else if(empCheck==IS_PART_TIME) {
+			System.out.println("Employee is present for half time");
+			empHours=4;
 		}
 		else{
 			System.out.println("Employee is Absent");
